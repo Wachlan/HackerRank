3c0731721164811c09a956087d6892230906d7cb
Wachlan <wachlanchow@gmail.com>
2021-04-27 22:01:21 +1000

    Update stacksAndQueues1.py
    Updated function methodology to look for opening and closing brackets.

--- stacksAndQueues1.py
+++ stacksAndQueues1.py
@@ -8,6 +8,25 @@ import sys
 
 # Complete the isBalanced function below.
 def isBalanced(s):
+    halfwayFlag = 0
+
+    for i in range(len(s)):
+        if s[i] == ')' or ']' or '}':
+            halfwayFlag = 1
+            break
+        elif s[i] == '(' or '[' or '{':
+            continue
+    
+    while i < len(s):
+        if s[i] == '(' or '[' or '{':
+            answer = "NO"
+            break
+        elif s[i] == ')' or ']' or '}':
+            continue
+
+    answer = "YES"
+
+    return answer
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
