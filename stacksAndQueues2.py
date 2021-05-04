6601a02043be9cfde32c0d1b88c9c82d708eae5c
Wachlan <wachlanchow@gmail.com>
2021-05-04 22:13:12 +1000

    Bugs Fixed
    Work in progress

--- stacksAndQueues2.py
+++ stacksAndQueues2.py
@@ -5,10 +5,11 @@ import os
 import random
 import re
 import sys
+from queue import Queue
 
 class MyQueue(object):
     def __init__(self):
-    	self.value = []
+    	self.value = Queue()
         
     
     def peek(self):                        #Enqueue element into the end of the queue.
@@ -16,17 +17,30 @@ class MyQueue(object):
         
     def pop(self):                         #dequeue the element at the front of the queue
         tempStack = []
-        while self.value.empty() != 1:
+        while self.value.isEmpty() != 1:
         	top = self.value.pop()
         	tempStack.append(top)
 
         tempStack.pop
 
-        while tempStack.empty() != 1:
-        	
+        while tempStack.isEmpty() != 1:
+        	top = tempStack.value.pop()
+        	self.value.append(top)
         
-    def put(self, value):
-        
+    def put(self, value):                  #Print the element at the front of the queue.
+        tempStack = []
+        while self.value.isEmpty() != 1:
+        	top = self.value.pop()
+        	tempStack.append(top)
+
+        frontElement = tempStack.pop()     #pop the element to be printed out
+        print(frontElement)                #print the first element from the queue
+        self.value.append(frontElement)    #put the element back into the 'queue'
+
+        while tempStack.isEmpty() != 1:
+        	top = tempStack.value.pop()
+        	self.value.append(top)
+
 
 queue = MyQueue()
 t = int(input())
